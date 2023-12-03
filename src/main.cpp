2a48a370c2ed187afe28042ba1f10f709895ddb7
Shmuel Berman <shmuelaberman@gmail.com>
2023-12-03 10:50:17 -0500

    added shell
    merge

--- src/main.cpp
+++ src/main.cpp
@@ -4,77 +4,26 @@
 #include <sstream>
 #include <algorithm>
 #include <bitset>
+#include "shell.h"
+
 
 int main() {
     // test
     start_music_thread();
+     std::map<CommandKey, Command> commands;
 
-    std::string input;
-    int number;
-    int count = 0;
- 
-while (true) {
-    std::string input;
-    int rhythmNumber;
-    float pitchNumber;
+    // Registering commands
+    commands[{"ci", 4}] = Command{"create_instrument", 4, executeCreateInstrument, "Create an instrument"};
+    commands[{"cr", 2}] = Command{"create_rhythm", 2, executeCreateRhythm, "Create a rhythm"};
+    commands[{"ls", 0}] = Command{"ls", 0, listFiles, "List files in the current directory"};
+    commands[{"greet", 1}] = Command{"greet", 1, greet, "Greet a user by name"};
+    commands[{"help", 0}] = Command{"help", 0, [commands](auto args){ showHelp(args, commands); }, "Show help text"};
+ while (true) {
+        std::cout << "> ";
+        std::string input;
+        std::getline(std::cin, input);
 
-    std::cout << "Enter an 8-bit binary string (or 'exit' to quit): ";
-    std::getline(std::cin, input);
-
-    // Check if the user wants to exit
-    if (input == "exit") {
-        break;
+        executeCommand(input, commands);
     }
-
-    if (input.length() != 8) {
-        std::cout << "Invalid input, enter an 8-bit binary string." << std::endl;
-        continue;
-    }
-  
-    // Reverse so plays in order of input
-    std::reverse(input.begin(), input.end());
-    std::bitset<8> binaryRepresentation(input);
-    unsigned char character = static_cast<unsigned char>(binaryRepresentation.to_ulong());
-
-    std::cout << "Enter a pitch number: ";
-    std::getline(std::cin, input);
-
-    // Using stringstream to convert string to number for pitch
-    std::stringstream ssPitch(input);
-    if (ssPitch >> pitchNumber) {
-        std::cout << "Pitch number entered: " << pitchNumber << std::endl;
-    } else {
-        std::cout << "Invalid input for pitch. Please enter a valid number." << std::endl;
-        continue; // Go to the next iteration of the loop
-    }
-
-    create_rhythm(character, ++count);
-    create_instrument("./samples/big snare.wav", count, count, pitchNumber);
-    std::cout << "Created instrument " << count << " with rhythm " << input << std::endl;
-    /*
-        void change_rhythm_pattern(char pattern, int rhythm_id);
-        void change_instrument_file(const std::string& filepath, int instrument_id);
-        void change_instrument_pitch(float pitch, int instrument_id);
-        char get_instrument_rhythm(int instrument_id);
-    */
-
-    // Testing Get_instrument_rhythm
-    char rhyt = get_instrument_rhythm(count);
-    std::bitset<8> rhytbinary(rhyt);
-    std::cout << "Testing get instr rhyt -- rhythm: " << rhytbinary << std:: endl;
-
-    // Testing change_rhythm_pattern
-    change_rhythm_pattern('A', count);
-    rhyt = get_instrument_rhythm(count);
-    std::bitset<8> rhytbinary2(rhyt);
-    std::cout << "Testing change rhyt -- new rhythm: " << rhytbinary2 << std:: endl;
-
-    // Testing change_instrument_pitch
-    change_instrument_pitch(10, count);
-
-    // Testing change_instrument_file
-    change_instrument_file("./samples/cocoon_perc.wav", count);
-    }
-
 }
 
---
+++ src/shell.cpp
@@ -0,0 +1,81 @@
+#include "shell.h"
+#include "music.h"
+
+enum class ArgType { INT, FLOAT, STRING };
+struct TypedArg {
+    ArgType type;
+    std::string value;
+};
+
+
+void executeCommand(const std::string& input, const std::map<CommandKey, Command>& commands) {
+    auto tokens = split(input, ' ');
+    if (tokens.empty()) return; // No input
+
+    std::string cmdName = tokens[0];
+    int argCount = tokens.size() - 1; // Number of arguments
+
+    auto it = commands.find({cmdName, argCount});
+    if (it == commands.end()) {
+        // Try finding a command with variable arguments (-1)
+        it = commands.find({cmdName, -1});
+    }
+
+    if (it != commands.end()) {
+        tokens.erase(tokens.begin()); // Remove command name from the list of arguments
+        it->second.func(tokens); // Execute the command
+    } else {
+        std::cout << "Error: Unknown command or incorrect number of arguments '" << cmdName << "'\n";
+    }
+}
+
+//Commands:
+void executeCreateInstrument(const std::vector<std::string>& stringArgs) {
+    if (stringArgs.size() != 4) {
+        std::cout << "Error: Incorrect number of arguments for create_instrument\n";
+        return;
+    }
+    try {
+        create_instrument(
+            "../samples/" + stringArgs[0],
+            std::stoi(stringArgs[1]),
+            std::stoi(stringArgs[2]),
+            std::stoi(stringArgs[3])
+        );
+    } catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << "\n";
+    }
+}
+
+
+void executeCreateRhythm(const std::vector<std::string>& stringArgs) {
+    if (stringArgs.size() != 2) {
+        std::cout << "Error: Incorrect number of arguments for create_rhythm\n";
+        return;
+    }
+    try {
+        create_rhythm(
+            stringArgs[0][0],
+            std::stoi(stringArgs[1])
+        );
+    } catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << "\n";
+    }
+}
+void listFiles(std::vector<std::string> args) {
+    // Implementation for listing files
+    std::cout << "Listing files...\n";
+}
+
+void greet(std::vector<std::string> args) {
+    if (!args.empty()) {
+        std::cout << "Hello, " << args[0] << "!\n";
+    }
+}
+
+void showHelp(std::vector<std::string> args, const std::map<CommandKey, Command>& commands) {
+    for (const auto& cmd : commands) {
+        std::cout << cmd.first.first << " - " << cmd.second.helpText << "\n";
+    }
+}
+
