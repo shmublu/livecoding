9ca578ddc7eb823d5990ecbb7286f79e660730d7
Dalia Etessami <daliaetess@gmail.com>
2023-12-06 00:05:16 -0500

    attempting to change int identifiers to strings

--- src/instrument.h
+++ src/instrument.h
@@ -25,14 +25,14 @@ private:
 class Instrument {
 public:
     std::string filepath;
-    int rhythm_id;
+    const std::string& rhythm_id;
     float pitch;
     Player player;
 
-    Instrument(const std::string& path, int id, float pitch);
+    Instrument(const std::string& path, std::string& id, float pitch);
     void play();
 
-    static Instrument& getInstrument(std::unordered_map<int, Instrument>& instruments, int instrument_id, float pitchVal);
+    static Instrument& getInstrument(std::unordered_map<int, Instrument>& instruments, std::string& instrument_id, float pitchVal);
 
 
 };
--- src/music.h
+++ src/music.h
@@ -11,17 +11,17 @@
 struct Rhythm {
     char pattern;
 };
-extern std::unordered_map<int, Instrument> instruments;
-extern std::unordered_map<int, Rhythm> rhythms;
+extern std::unordered_map<std::string&, Instrument> instruments;
+extern std::unordered_map<std::string&, Rhythm> rhythms;
 extern std::mutex state_mutex;
 
 void start_music_thread();
-void create_instrument(const std::string& filepath, int rhythm_id, int instrument_id, int pitch);
-void create_rhythm(char pattern, int rhythm_id);
+void create_instrument(const std::string& filepath, const std::string& rhythm_id, const std::string& instrument_id, int pitch);
+void create_rhythm(char pattern, const std::string& rhythm_id);
 
-void change_rhythm_pattern(char pattern, int rhythm_id);
-void change_instrument_file(const std::string& filepath, int instrument_id);
-void change_instrument_pitch(float pitch, int instrument_id);
-char get_instrument_rhythm(int instrument_id);
+void change_rhythm_pattern(char pattern, const std::string& rhythm_id);
+void change_instrument_file(const std::string& filepath, const std::string& instrument_id);
+void change_instrument_pitch(float pitch, std::string& instrument_id);
+char get_instrument_rhythm(std::string& instrument_id);
 
 #endif // MUSIC_H
